7bcade64f6594f7e002c2a04591c65fee4f21431
relund <lars@relund.dk>
2015-06-08 17:38:11 +0200

    Update desc

--- paper/run.R
+++ paper/run.R
@@ -5,7 +5,7 @@
 ## 3) Plot the results (stored in pdf files)
 
 source("optimize_hmdp.R")   # find optimal policy
-source("simulate.R")        # simulate the 3 pens based on the optimal policy
+source("simulate.R")        # simulate the 3 pens (use optimal policy to identify feed-mix and number of pigs)
 source("plot.R")            # plot the results
 
 
